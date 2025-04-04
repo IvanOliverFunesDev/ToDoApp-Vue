558f99a8d7e99e6265d2e55e94ce5cd41b0ca081
IvanOliverFunesDev <ioliverfunes@gmail.com>
2025-04-04 11:27:29 +0200

    feat: crear page para login

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="login-page">
+        <h1>Registro</h1>
+        <form @submit.prevent="handleSubmit">
+            <input type="email" placeholder="Correo electrónico" v-model="email" required />
+            <input type="password" placeholder="Contraseña" v-model="password" required />
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { login } from '../services/authService'
+
+const email = ref('')
+const password = ref('')
+
+const router = useRouter()
+
+const handleSubmit = async () => {
+    try {
+        const data = await login({ email: email.value, password: password.value })
+        console.log("✅ Usuario registrado:", data)
+
+        router.push('/login')
+    } catch (err) {
+        console.error("❌ Error al registrar:", err.response?.data || err.message)
+    }
+}
+</script>
+
+<style scoped>
+.login-page {
+    max-width: 400px;
+    margin: 2rem auto;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+
+input,
+button {
+    padding: 0.5rem;
+    font-size: 1rem;
+}
+</style>
