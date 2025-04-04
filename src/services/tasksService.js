30a88502cc2ec2b069cad7d48bac04a46bfe76f1
IvanOliverFunesDev <ioliverfunes@gmail.com>
2025-04-04 12:47:40 +0200

    feat: crear flujo completo para crateTask

--- src/services/tasksService.js
+++ src/services/tasksService.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const API_URL = 'http://localhost/api';
 
 export const createTask = async (taskData) => {
+    const token = localStorage.getItem('token')
+
     const response = await axios.post(`${API_URL}/tasks`, taskData, {
         headers: {
             Authorization: `Bearer ${token}`
